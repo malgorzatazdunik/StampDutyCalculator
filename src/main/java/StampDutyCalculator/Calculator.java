8845c47ca5844e63019ee9a5955cad55bf5129a0
goszk <g.zdunik@gmail.com>
2020-12-31 15:25:40 +0000

    minor changes and comments

--- src/main/java/StampDutyCalculator/Calculator.java
+++ src/main/java/StampDutyCalculator/Calculator.java
@@ -18,9 +18,16 @@ public class Calculator {
 		
 	}
 	
-	boolean isValidInput()
+	/*
+	 * isValidInput checks if the input values of price and country are valid. It sets the boolean validInput to false or true depending on whether
+	 * input is valid or not
+	 */
+	
+	private boolean isValidInput()
 	{
 		boolean ans = true;
+		
+		// if price is below 0, input is invalid
 		if (price < 0)
 		{
 			ans = false;
@@ -28,6 +35,7 @@ public class Calculator {
 		
 		Set<String> countries = new HashSet<>(Arrays.asList("England", "N.Ireland","Scotland", "Wales"));
 		
+		// if country is not set to any of the valid countries, input is invalid
 		if (!countries.contains(country))
 		{
 			ans = false;
@@ -37,34 +45,40 @@ public class Calculator {
 	}
 	
 	
-	
+	/*
+	 * calculateStampDuty is a public method that can be called using the Calculator object. It checks whether the input is valid and calls
+	 * calculate(double[] thresholds) with the appropriate array of thresholds depending on the country. Returns stamp duty (double). 
+	 */
 	public double calculateStampDuty()
+	
 	{
+		double[] thresholds;
+		
+		// if input is valid, proceed to calculate stamp duty
 		if (validInput)
 		{
 			
 			if (country.equals("England") || country.contentEquals("N.Ireland"))
 			{
-				double[] tresholds = {500000, 925000, 1500000};
-				return calculate(tresholds);
-				//return calculateEnglandNIreland();
+				thresholds = new double[]{500000, 925000, 1500000};
+				return calculate(thresholds);
 			}
 			
 			else if (country.contentEquals("Scotland"))
 			{
-				double[] tresholds = {250000,325000,750000};
-				return calculate(tresholds);
-				//return calculateScotland();
+				thresholds = new double[]{250000,325000,750000};
+				return calculate(thresholds);
 			}
 			
 			else //Wales
 			{
-				double[] tresholds = {250000,400000,750000,1500000};
-				return calculate(tresholds);
+				thresholds = new double[]{250000,400000,750000,1500000};
+				return calculate(thresholds);
 			}
 			
 		}
 		else {
+			// if input is invalid, return -1
 			System.out.println("Invalid Input");
 			return -1;
 		}
@@ -73,74 +87,79 @@ public class Calculator {
 		
 	}
 	
-	private double calculate(double[] tresholds)
+	/*
+	 * calculate(double[] thresholds) calculates stamp duty and the percentage of effective stamp duty rate depending on the country and price
+	 * of the house. Returns stamp duty (double).
+	 */
+	private double calculate(double[] thresholds)
 	{
 		
 		double answer = 0;
 		
-		if (tresholds.length == 3)
+		// England, N.Ireland and Scotland have 3 thresholds to consider
+		if (thresholds.length == 3)
 		{
-			double aboveSecond = tresholds[1] - tresholds[0];
-			double aboveThird = tresholds[2] - tresholds[1];
+			double aboveSecond = thresholds[1] - thresholds[0];
+			double aboveThird = thresholds[2] - thresholds[1];
 			
 			
-			
-			if (price <= tresholds[0])
+			if (price <= thresholds[0])
 			{
 				return answer;
 			}
 			
-			else if (price > tresholds[0] && price <= tresholds[1])
+			else if (price > thresholds[0] && price <= thresholds[1])
 			{
-				double aboveLast = price - tresholds[0];
+				double aboveLast = price - thresholds[0];
 				answer = aboveLast * 5/100;
 			}
 			
-			else if (price > tresholds[1] && price <= tresholds[2])
+			else if (price > thresholds[1] && price <= thresholds[2])
 			{
-				double aboveLast = price - tresholds[1];
+				double aboveLast = price - thresholds[1];
 				answer = aboveSecond * 5/100 + aboveLast * 10/100;
 			}
 			
 			else {
-				double aboveLast = price - tresholds[2];
+				double aboveLast = price - thresholds[2];
 				answer = aboveSecond * 5/100 + aboveThird * 10/100 + aboveLast * 12/100;
 			}
 			
 		}
 		
-		if (tresholds.length == 4)
+		// Wales has 4 thresholds to consider
+		if (thresholds.length == 4)
 		{
 			
-			double aboveSecond = tresholds[1] - tresholds[0];
-			double aboveThird = tresholds[2] - tresholds[1];
-			double aboveFourth = tresholds[3] - tresholds[2];
+			double aboveSecond = thresholds[1] - thresholds[0];
+			double aboveThird = thresholds[2] - thresholds[1];
+			double aboveFourth = thresholds[3] - thresholds[2];
 			
 			
 			
-			if (price <= tresholds[0])
+			if (price <= thresholds[0])
 			{
 				return answer;
 			}
 			
-			else if (price > tresholds[0] && price <= tresholds[1])
+			else if (price > thresholds[0] && price <= thresholds[1])
 			{
-				double aboveLast = price - tresholds[0];
+				double aboveLast = price - thresholds[0];
 				answer = aboveLast * 5/100;
 			}
 			
-			else if (price > tresholds[1] && price <= tresholds[2])
+			else if (price > thresholds[1] && price <= thresholds[2])
 			{
-				double aboveLast = price - tresholds[1];
+				double aboveLast = price - thresholds[1];
 				answer = aboveSecond * 5/100 + aboveLast * 7.5/100;
 			}
 			
-			else if (price > tresholds[2] && price <= tresholds[3]){
-				double aboveLast = price - tresholds[2];
+			else if (price > thresholds[2] && price <= thresholds[3]){
+				double aboveLast = price - thresholds[2];
 				answer = aboveSecond * 5/100 + aboveThird * 7.5/100 + aboveLast * 10/100;
 			}
 			else {
-				double aboveLast = price - tresholds[3];
+				double aboveLast = price - thresholds[3];
 				answer = aboveSecond * 5/100 + aboveThird * 7.5/100 + aboveFourth * 10/100 + aboveLast * 12/100;
 			}
 			
@@ -149,6 +168,7 @@ public class Calculator {
 		
 		double percent = answer/price * 100;
 		
+		// Print stamp duty and percentage of effective rate (rounded)
 		System.out.println("Your effective stamp duty rate is " + Math.round(percent*100.0)/100.0 + "%");
 		System.out.println("You will have to pay £" + Math.round(answer*100.0)/100.0 + " in Stamp Duty");
 		return answer;
--- src/main/java/StampDutyCalculator/Main.java
+++ src/main/java/StampDutyCalculator/Main.java
@@ -5,15 +5,11 @@ public class Main {
 	
 	public static void main(String[] args)
 	{
-		float price1 = 125000;
-		float price2 = 250000;
-		float price3 = 500000;
-		float price4 = 650000;
-		float price5 = -100;
-		float price6 = 1549876;
-		Calculator calculator = new Calculator(price5, "Wales");
+
+		float price = 1549876;
+		Calculator calculator = new Calculator(price, "Wales");
 		
-		System.out.println(calculator.calculateStampDuty());
+		calculator.calculateStampDuty();
 	}
 
 }
